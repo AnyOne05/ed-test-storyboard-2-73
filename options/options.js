45bd4a506306aac07d55c20a7cd512e48762f97d
EduardoWeber <eduardo-felipe-weber@hotmail.com>
2023-01-24 12:52:17 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ed-test-storyboard-2-73",
+  url: "https://ed-test-storyboard-2-73.botics.co",
+  api: "https://ed-test-storyboard-2-73.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
